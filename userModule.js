d9ddcf9e05b1c6841fd201c042e6a4a13c6a2393
sarojrani <sarojkumarirani345@gmail.com>
2023-04-03 15:25:48 +0530

    final

--- userModule.js
+++ userModule.js
@@ -3,35 +3,34 @@ const mongoose = require("mongoose");
 const userModel = new mongoose.Schema({
     firstName:{
         type:String,
-        required:true
+        
     },
     lastName:{
         type:String,
-        required:true
+     
     },
     address:{
         type:String,
-        required:true
+        
     },
     email:{
         type:String,
-        required:true,
+        
         unique:true
 
     },
     mobile:{
         type :String,
-        required:true,
         unique:true
 
     },
     DOB:{
         type:String,
-        required:true
+       
     },
     password:{
         type:String,
-        required:true
+       
     }
 
 
